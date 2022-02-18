20a33f930acd581831ce3eda4bc58bf28978e11f
Jas1nskas <Edvinasjasinskas@gmail.com>
2022-02-18 13:17:57 +0200

    update css

--- src/Comps/CreateRecipe.js
+++ src/Comps/CreateRecipe.js
@@ -5,14 +5,14 @@ const UserInfo = ({recipe}) => {
 
 
     return (
-        <div className="d-flex center" >
+        <div className=" box d-flex center column">
 
             <img src={recipe.photo} alt=""/>
+            <img src={recipe.secPhoto} alt=""/>
             <h1>{recipe.title}</h1>
             <div>{recipe.ingredients}</div>
             <div>{recipe.preTime}</div>
             <div>{recipe.preStep}</div>
-
 
 
         </div>
